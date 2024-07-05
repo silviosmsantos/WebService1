fdc551625aa8b2015e7f183327fd662a6f543d64
Silvio Martins <silviosmsantos@gmail.com>
2024-07-05 18:05:53 -0300

    construction order status enum

--- webservice/src/main/java/com/santos/webservice/config/TestConfig.java
+++ webservice/src/main/java/com/santos/webservice/config/TestConfig.java
@@ -2,6 +2,7 @@ package com.santos.webservice.config;
 
 import com.santos.webservice.entities.Order;
 import com.santos.webservice.entities.User;
+import com.santos.webservice.entities.enums.OrderStatus;
 import com.santos.webservice.repositories.OrderRepository;
 import com.santos.webservice.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,12 +28,12 @@ public class TestConfig implements CommandLineRunner {
         User u1 = new User(null, "Silvio Martins Santos","88998071000","silvio@gmail.com","123456");
         User u2 = new User(null, "Joicy Peixoto Dias","88998071001" ,"jane@example.com", "54321");
 
-        Order o1 = new Order(null, Instant.parse("2024-06-29T15:40:07Z"), u1);
-        Order o2 = new Order(null, Instant.parse("2024-06-30T12:40:07Z"), u1);
-        Order o3 = new Order(null, Instant.parse("2024-07-04T10:00:07Z"), u1);
+        Order o1 = new Order(null, Instant.parse("2024-06-29T15:40:07Z"), OrderStatus.WAITING_PAYMENT,u1);
+        Order o2 = new Order(null, Instant.parse("2024-06-30T12:40:07Z"), OrderStatus.PAID,u1);
+        Order o3 = new Order(null, Instant.parse("2024-07-04T10:00:07Z"), OrderStatus.PAID,u1);
 
-        Order o4 = new Order(null, Instant.parse("2024-06-30T15:40:07Z"), u2);
-        Order o5 = new Order(null, Instant.parse("2024-07-01T09:20:07Z"), u2);
+        Order o4 = new Order(null, Instant.parse("2024-06-30T15:40:07Z"), OrderStatus.PAID,u2);
+        Order o5 = new Order(null, Instant.parse("2024-07-01T09:20:07Z"), OrderStatus.PAID,u2);
         //instant.parse() is using ISO 8161 type
 
 
--- webservice/src/main/java/com/santos/webservice/entities/Order.java
+++ webservice/src/main/java/com/santos/webservice/entities/Order.java
@@ -1,6 +1,7 @@
 package com.santos.webservice.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.santos.webservice.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -16,7 +17,7 @@ public class Order implements Serializable {
     private Long id;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
-
+    private Integer orderStatus;
     @ManyToOne
     @JoinColumn(name = "client_id")
     private User client;
@@ -24,9 +25,10 @@ public class Order implements Serializable {
     public Order (){
 
     }
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus,User client) {
         this.id = id;
         this.moment = moment;
+        setOrderStatus(orderStatus);
         this.client = client;
     }
 
@@ -46,6 +48,16 @@ public class Order implements Serializable {
         this.moment = moment;
     }
 
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null){
+            this.orderStatus = orderStatus.getCode();
+        }
+    }
+
     public User getClient() {
         return client;
     }
@@ -53,6 +65,8 @@ public class Order implements Serializable {
     public void setClient(User client) {
         this.client = client;
     }
+
+
 
     @Override
     public boolean equals(Object o) {
