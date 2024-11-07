3301aa4c98b12a9ac83311ca061151af6464fcb4
Silvio Martins <silviosmsantos@gmail.com>
2024-11-06 22:03:11 -0300

    fix: change config package

--- webservice/src/main/java/com/smsantos/webservice/entities/User.java
+++ webservice/src/main/java/com/smsantos/webservice/entities/User.java
@@ -12,8 +12,11 @@ public class User implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(length=200,nullable=false)
     private String name;
+    @Column(length=200,nullable=false)
     private String email;
+    @Column(length=30,nullable=false)
     private String phone;
     private String password;
 
--- webservice/src/main/java/com/smsantos/webservice/repositories/UserRepository.java
+++ webservice/src/main/java/com/smsantos/webservice/repositories/UserRepository.java
@@ -1,6 +1,7 @@
 package com.smsantos.webservice.repositories;
 
 import com.smsantos.webservice.entities.User;
+import jakarta.persistence.Id;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +9,3 @@ import org.springframework.stereotype.Repository;
 public interface UserRepository extends JpaRepository<User, Long> {
 
 }
-
