77e389b9680e9ee1f771479d152156ac0347636d
Silvio Martins <silviosmsantos@gmail.com>
2024-11-07 17:05:53 -0300

    feat: add JSON.format ISO 8601

--- webservice/src/main/java/com/smsantos/webservice/entities/Order.java
+++ webservice/src/main/java/com/smsantos/webservice/entities/Order.java
@@ -1,5 +1,6 @@
 package com.smsantos.webservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
@@ -15,6 +16,7 @@ public class Order implements Serializable {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'",timezone = "GMT")
     private Instant moment;
 
     @ManyToOne
