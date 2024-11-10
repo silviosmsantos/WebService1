c764f6e3e561703da3012c93d2b8543fadcdb15b
Silvio Martins <silviosmsantos@gmail.com>
2024-11-10 16:36:57 -0300

    feat: OrderItem associations classes parceally fineshed

--- webservice/src/main/java/com/smsantos/webservice/entities/Category.java
+++ webservice/src/main/java/com/smsantos/webservice/entities/Category.java
@@ -1,5 +1,6 @@
 package com.smsantos.webservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -20,7 +21,8 @@ public class Category implements Serializable {
      * associated with a category is unique, preventing multiple occurrences of
      * the same product within this category.</p>
      */
-    @Transient
+    @ManyToMany(mappedBy = "categories")
+    @JsonIgnore
     private Set<Product> products = new HashSet<>();
 
     public Category(){
