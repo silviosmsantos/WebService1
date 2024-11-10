f2d8e624fd915c214fc0d1e4051d6984e13c551b
Silvio Martins <silviosmsantos@gmail.com>
2024-11-10 17:51:11 -0300

    feat: association one to many for product-orderItem classes

--- webservice/src/main/java/com/smsantos/webservice/entities/OrderItem.java
+++ webservice/src/main/java/com/smsantos/webservice/entities/OrderItem.java
@@ -39,6 +39,7 @@ public class OrderItem implements Serializable {
         this.id.setOrder(order);
     }
 
+
     public Product getProduct(){
         return id.getProduct();
     }
--- webservice/src/main/java/com/smsantos/webservice/entities/Product.java
+++ webservice/src/main/java/com/smsantos/webservice/entities/Product.java
@@ -1,5 +1,6 @@
 package com.smsantos.webservice.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -27,6 +28,9 @@ public class Product implements Serializable{
     @ManyToMany
     @JoinTable( name = "tb_product_category", joinColumns = @JoinColumn(name = "product_id"), inverseJoinColumns = @JoinColumn(name = "category_id"))
     private Set<Category> categories = new HashSet<>();
+
+    @OneToMany(mappedBy = "id.product")
+    private Set<OrderItem> items = new HashSet<>();
 
     public Product (){
 
@@ -82,6 +86,15 @@ public class Product implements Serializable{
 
     public Set<Category> getCategories() {
         return categories;
+    }
+
+    @JsonIgnore
+    public Set<Order> getOrders(){
+        Set<Order> set = new HashSet<>();
+        for(OrderItem order: items){
+            set.add(order.getOrder());
+        }
+        return set;
     }
 
     @Override
